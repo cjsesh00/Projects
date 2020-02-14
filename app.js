6fd943e487ee703ef68e40c283d009412d73ddda
Zac Barreca <zacbarreca@gmail.com>
2020-02-14 15:33:35 -0500

    update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const STORE = {
       correct: 1
     }
   ],
-
+  quizStarted: false,
   questionNumber: 0,
   score: 0
 };
@@ -80,61 +80,6 @@ const STORE = {
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
  */
-
-function main() {
-  startQuiz();
-
-  console.log('main fired');
-}
-
-function renderPage() {
-  let html = '';
-
-  if (STORE.quizStarted === false) {
-    $('main').html(generateStartScreenHtml());
-    return;
-  }
-
-  else if (STORE.currentQuestion < STORE.questions.length) {
-    html = generateQuestionNumberAndScoreHtml();
-    html += generateQuestionHtml();
-    $('main').html(html);
-  }
-
-  // console.log('renderPage fired');
-}
-
-
-function startQuiz() {
-//displays intro and welcomes user to quiz
-  const intro = 
-  `
-  <div class = "start">
-  <h1>How well do you know your favorite cartoon characters</h1>
-  <a href =><input type= "Submit"></input></a>
-  <div>
-  `
-
-  $("main").html(intro)
-
-  console.log('startQuiz fired');
-}
-
-//function displayButtons() {
-  //generates radio buttons to be used as answer selection
-      //<input type="radio" id="answer1" name="answer1" value= 0>
-      //<label for=""></label><br>
-      //<input type="radio" id="answer2" name="answer2" value= 1>
-      //<label for=""></label><br>
-      //<input type="radio" id="answer3" name="answer3" value= 2>
-      //<label for=""></label><br>
-      //<input type="radio" id="answer4" name="answer4" value= 3>
-      //<label for=""></label><br>
-      
-      //<input type= "Submit"></input>
-
-  //console.log('displayButtons has fired');
-//}
 
 //function buttonSelection() {
 //delegates listener to radio buttons and returns input value
@@ -174,6 +119,103 @@ function startQuiz() {
 
 
 
+/******************* TEMPLATE GENERATION FUNCTIONS *******************/
+// These functions return HTML templates
+function generateStartScreenHtml(){
+  return `
+  <div class = "start-screen">
+  <h1>How well do you know your favorite cartoon characters</h1>
+  <a href =><input type= "Submit"></input></a>
+  <div>
+  `;
+}
 
+function generateQuestionNumberAndScoreHtml() {
+  return `
+  <ul class="question and score">
+    <li id="question-number">
+      Question Number: ${STORE.currentQuestion + 1}/${STORE.questions.length}
+    </li>
+    <li id="score">
+      Score: ${STORE.score}/${STORE.questions.length}
+    </li>
+  <ul>`
+}
 
-$(main);
+function generateAnswersHtml(){
+  const answersArray = STORE.questions[STORE.currentQuestion].answers;
+  let answersHtml = '';
+  let i = 0;
+
+  answersArray.forEach(answer => {
+    answersHtml += `
+    <div id="option-container-${i}">
+      <input type="radio" name="options" id="option${i+1}" value="${answer}" tabindex="${i + 1} required>
+      <label for="option${i +1}">${answer}</label>
+    </div>
+    `;
+    i++;
+  });
+  return answersHtml;
+}
+
+/******************* RENDER FUNCTION(S) *******************/
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+function render() {
+  let html = '';
+
+  if (STORE.quizStarted === false) {
+    $('main').html(generateStartScreenHtml());
+    return;
+  }
+
+  else if (STORE.currentQuestion < STORE.questions.length) {
+    html = generateQuestionNumberAndScoreHtml();
+    html += generateQuestionHtml();
+    $('main').html(html);
+  }
+  
+  else {
+    $('main').html(generateResultsScreen());
+  }
+  // console.log('render fired');
+}
+
+/******************* EVENT HANDLER FUNCTIONS *******************/
+// These functions handle events (submit, click, etc)
+function handleStartClick(){
+  $('main').on('click', '#start', function(event) {
+    STORE.quizStarted = true;
+    render();
+  });
+}
+
+function handleNextQuestionClick(){
+  $('main').on('click', function(event) {
+    
+  });
+}
+
+function handleQuestionFormSubmission(){
+
+}
+
+function handleRestartButtonClick(){
+
+}
+
+function handleQuizApp() {
+  render();
+  handleStartClick();
+  handleNextQuestionClick();
+  handleQuestionFormSubmission();
+  handleRestartButtonClick();
+}
+
+function restartQuiz(){
+  STORE.quizStarted = false;
+  STORE.currentQuestion = 0;
+  STORE.score = 0;
+}
+
+$(handleQuizApp);
